432ee368d263b32c110e3bacf0ec3892382ca0bf
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-08-16 13:37:41 +0200

    created my_select & my-all  methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,20 +1,61 @@
 module Enumerable
-    def my_each(arr)
-        j = 0
-        while j <= arr.length-1
-            yield(arr[j])
-            j += 1
+    def my_each
+        return to_enum(:my_each) unless block_given?
+        n = 0
+        while n < self.size
+            yield(self[n])
+            n += 1
         end
+        self
     end
 
-    def my_each_with_index(arr)
-        i = 0
-        j = 0
-        while j <= arr.length-1
-            yield(i) 
-            yield(arr[j])
-            j += 1
-            i += 1
+    def my_each_with_index
+        return to_enum(:my_each_with_index) unless block_given?
+        n = 0
+        x = 0
+        while n < self.size
+            yield(self[n] ,x)
+            n += 1
+            x += 1
+
         end
+        self
+    end
+    def my_select
+        return to_enum(:my_select) unless block_given?
+        n = 0
+        outcome = []
+        while n < self.size
+            if yield(self[n])
+                outcome.push(self[n])
+            end
+            n += 1
+        end
+        outcome
+    end
+    def my_all
+        return to_enum(:my_all) unless block_given?
+        n = 0
+        x = 0
+        outcome = []
+
+        while n < self.size
+            if yield(self[n])
+                outcome.push(true)
+            else 
+                outcome.push(false)
+            end
+            n += 1
+        end
+        result = true
+        while x < outcome.size
+            if outcome[x] == false 
+                result = false
+            end
+            x += 1
+        end
+        result
+        
     end
 end
+include Enumerable
--- main.rb
+++ main.rb
@@ -1,34 +1,6 @@
-=begin require_relative 'enumerables.rb' 
-=end
-
-my_array = [3,5,7,9]
-
-def my_each(arr)
-    j = 0
-    while j <= arr.length-1
-        yield(arr[j])
-        j += 1
-    end
-end
-
-puts my_array.my_each {|j|}
-
-
-
-=begin
-my_array = [3,5,7,9]
-
-def my_each_with_index(my_array)
-    i = 0
-    j = 0
-    while j <= my_array.length-1
-        yield(i) 
-        yield(my_array[j])
-        j += 1
-        i += 1
-        puts i, j
-    end
-end
-
-puts my_each_with_index(my_array){|i,j|} 
-=end
+ require_relative 'enumerables.rb' 
+my_array = [2,6,8,10]
+# my_array.my_each{|i| p i}
+# my_array.my_each_with_index{|i,j|p "#{j} : #{i}" }
+# p my_array.my_select{|i| i.even?}
+# p my_array.my_all{|i| i.even?}
